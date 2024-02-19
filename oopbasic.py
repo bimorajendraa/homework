df2f10ca7d59d2b7ce1261b08e68cf1ab3ae884c
bimorajendraa <87067815+bimorajendraa@users.noreply.github.com>
2024-02-19 23:02:17 +0700

    Add files via upload

---
+++ oopbasic.py
@@ -0,0 +1,18 @@
+class Student():
+
+    # constructor
+    def __init__(self, name, birthday, faculty):
+        self.name = name
+        self.birthday = birthday
+        self.faculty = faculty
+    
+    def attend_class(self, subject):
+        print("{0} is attending {1} class".format(self.name, subject))
+
+john = Student(name="John", birthday="26/04/2000", faculty="Science")
+dewi = Student(name="Dewi", birthday="29/05/1998", faculty="Computer science")
+
+print(john.faculty)
+print(dewi.birthday)
+
+dewi.attend_class("Python for data")
